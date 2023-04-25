244cf1bbb6759e11642e8c435d735cc5cb3a0a07
Timothy Adamcik <tadamcik0425@gmail.com>
2023-04-25 12:59:05 -0400

    add uicolor extension and theme color/font structs

---
+++ tip-calculator/config/ThemeFont.swift
@@ -0,0 +1,23 @@
+//
+//  ThemeFont.swift
+//  tip-calculator
+//
+//  Created by Timothy Adamcik on 4/25/23.
+//
+
+import UIKit
+
+struct ThemeFont {
+    // AvenirNext Family
+    static func regular(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Regular", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func bold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Bold", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func demiBold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-DemiBold", size: size) ?? .systemFont(ofSize: size)
+    }
+}
