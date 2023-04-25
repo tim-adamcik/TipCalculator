bd348b5be014693d881bfeb24b82eebf464c0287
Timothy Adamcik <tadamcik0425@gmail.com>
2023-04-25 12:25:47 -0400

    Add stackview with view components to calculator vc, add layout funcs to every view and arrange subviews on main vc

---
+++ tip-calculator/CalculatorVC.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by Timothy Adamcik on 4/24/23.
+//
+
+import UIKit
+import SnapKit
+
+class CalculatorVC: UIViewController {
+
+    private let logoView = LogoView()
+    private let resultView = ResultView()
+    private let billInputView = BillInputView()
+    private let tipInputView = TipInputView()
+    private let splitInputView = SplitInputLogoView()
+    
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            logoView,
+            resultView,
+            billInputView,
+            tipInputView,
+            splitInputView
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = 36
+        return stackView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        layout()
+    }
+
+    private func layout() {
+        view.backgroundColor = .white
+        view.addSubview(vStackView)
+        
+        vStackView.snp.makeConstraints({ make in
+            make.leading.equalTo(view.snp.leadingMargin).offset(16)
+            make.trailing.equalTo(view.snp.trailingMargin).offset(-16)
+            make.top.equalTo(view.snp.topMargin).offset(16)
+            make.bottom.equalTo(view.snp.bottomMargin).offset(-16)
+        })
+        
+        logoView.snp.makeConstraints({ make in
+            make.height.equalTo(48)
+        })
+        
+        resultView.snp.makeConstraints({ make in
+            make.height.equalTo(224)
+        })
+        
+        billInputView.snp.makeConstraints({ make in
+            make.height.equalTo(56)
+        })
+        
+        tipInputView.snp.makeConstraints({ make in
+            make.height.equalTo(56+56+16)
+        })
+        
+        splitInputView.snp.makeConstraints({ make in
+            make.height.equalTo(56)
+        })
+    }
+
+}
+
---
+++ tip-calculator/View/SplitInputView.swift
@@ -0,0 +1,26 @@
+//
+//  SplitInputView.swift
+//  tip-calculator
+//
+//  Created by Timothy Adamcik on 4/25/23.
+//
+
+import Foundation
+import UIKit
+
+class SplitInputLogoView: UIView {
+    
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .systemBlue
+    }
+}
