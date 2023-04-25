61fa9797fbe693941be10b570e0ec73c51315bd2
Timothy Adamcik <tadamcik0425@gmail.com>
2023-04-25 14:20:15 -0400

    added horiz and vert stack view to result view

--- tip-calculator/View/ResultView.swift
+++ tip-calculator/View/ResultView.swift
@@ -10,6 +10,44 @@ import UIKit
 
 class ResultView: UIView {
     
+    private let headerLabel: UILabel = {
+        LabelFactory.build(text: "Total p/person", font: ThemeFont.demiBold(ofSize: 18))
+    }()
+    
+    private let amountPerPersonLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        let text = NSMutableAttributedString(string: "$0", attributes: [.font: ThemeFont.bold(ofSize: 48)])
+        text.addAttributes([.font: ThemeFont.bold(ofSize: 24)], range: NSMakeRange(0, 1))
+        label.attributedText = text
+        return label
+    }()
+    
+    private let horizontalLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = ThemeColor.separator
+        return view
+    }()
+    
+    private lazy var hStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+
+        ])
+        view.axis = .vertical
+        view.spacing = -4
+        view.alignment = .center
+        return view
+    }()
+    
+    private lazy var vStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+
+        ])
+        view.axis = .vertical
+        view.spacing = -4
+        view.alignment = .center
+        return view
+    }()
     
     init() {
         super.init(frame: .zero)
