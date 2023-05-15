176afb71a459636170a56c50736641182f8cd661
Timothy Adamcik <tadamcik0425@gmail.com>
2023-05-15 15:48:47 -0400

    Create UI for tip input view, create enum for Tip, arrange subviews

--- tip-calculator/View/TipInputView.swift
+++ tip-calculator/View/TipInputView.swift
@@ -10,6 +10,59 @@ import UIKit
 
 class TipInputView: UIView {
     
+    private let headerView: UIView = {
+        let view = HeaderView()
+        view.configure(topText: "Choose", bottomText: "your tip")
+        return view
+    }()
+    
+    private lazy var tenPercentButton: UIButton = {
+        let button = buildTipButton(tip: .tenPercent)
+        return button
+    }()
+    
+    private lazy var fifteenPercentButton: UIButton = {
+        let button = buildTipButton(tip: .fifteenPercent)
+        return button
+    }()
+    
+    private lazy var twentyPercentButton: UIButton = {
+        let button = buildTipButton(tip: .twentyPercent)
+        return button
+    }()
+    
+    private lazy var horizontalStackView: UIStackView = {
+       let view = UIStackView(arrangedSubviews: [
+        tenPercentButton,
+        fifteenPercentButton,
+        twentyPercentButton
+       ])
+        view.axis = .horizontal
+        view.distribution = .fillEqually
+        view.spacing = 16
+        return view
+    }()
+    
+    private lazy var customTipButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Custom Tip", for: .normal)
+        button.titleLabel?.font = ThemeFont.bold(ofSize: 20)
+        button.backgroundColor = ThemeColor.primary
+        button.tintColor = .white
+        button.addCornerRadius(radius: 8.0)
+        return button
+    }()
+    
+    private lazy var vStackView: UIStackView = {
+       let view = UIStackView(arrangedSubviews: [
+        horizontalStackView,
+        customTipButton
+       ])
+        view.axis = .vertical
+        view.spacing = 16
+        view.distribution = .fillEqually
+        return view
+    }()
     
     init() {
         super.init(frame: .zero)
@@ -21,6 +74,29 @@ class TipInputView: UIView {
     }
     
     private func layout() {
-        backgroundColor = .systemPink
+        [headerView, vStackView].forEach(addSubview(_:))
+        
+        vStackView.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview()
+        }
+        
+        headerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.trailing.equalTo(vStackView.snp.leading).offset(-24)
+            make.width.equalTo(68)
+            make.centerY.equalTo(horizontalStackView.snp.centerY)
+        }
+        
+    }
+    
+    private func buildTipButton(tip: Tip) -> UIButton {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = ThemeColor.primary
+        button.addCornerRadius(radius: 8.0)
+        let text = NSMutableAttributedString(string: tip.stringValue, attributes: [.font: ThemeFont.bold(ofSize: 20),
+                                                                                   .foregroundColor: UIColor.white])
+        text.addAttributes([.font: ThemeFont.demiBold(ofSize: 14)], range: NSMakeRange(2, 1))
+        button.setAttributedTitle(text, for: .normal)
+        return button
     }
 }
