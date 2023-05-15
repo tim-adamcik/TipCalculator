0dd6fccd5f2d44ec144e9d24f3aee54f6f415de7
Timothy Adamcik <tadamcik0425@gmail.com>
2023-05-15 16:41:11 -0400

    Add rounded corners to view extension, set up split input views with masked corners and format buttons/arrange subviews

--- tip-calculator/View/SplitInputView.swift
+++ tip-calculator/View/SplitInputView.swift
@@ -10,7 +10,40 @@ import UIKit
 
 class SplitInputLogoView: UIView {
     
+    private let headerView: HeaderView = {
+        let view = HeaderView()
+        view.configure(topText: "Split", bottomText: "the total")
+        return view
+    }()
     
+    private lazy var decrementButton: UIButton = {
+        let button = buildButton(text: "-", cornerMask: [.layerMinXMaxYCorner,.layerMinXMinYCorner])
+        return button
+    }()
+    
+    private lazy var incrementButton: UIButton = {
+        let button = buildButton(text: "+", cornerMask: [.layerMaxXMinYCorner,.layerMaxXMaxYCorner])
+        return button
+    }()
+    
+    private lazy var quantityLabel: UILabel = {
+        let label = LabelFactory.build(text: "1", font: ThemeFont.bold(ofSize: 20))
+        label.backgroundColor = .white
+        return label
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+            decrementButton,
+            quantityLabel,
+            incrementButton
+        ])
+        view.axis = .horizontal
+        view.spacing = 0
+        return view
+    }()
+
+
     init() {
         super.init(frame: .zero)
         layout()
@@ -21,6 +54,33 @@ class SplitInputLogoView: UIView {
     }
     
     private func layout() {
-        backgroundColor = .systemBlue
+        [headerView, stackView].forEach(addSubview(_:))
+        
+        stackView.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview()
+        }
+        
+        [incrementButton, decrementButton].forEach { button in
+            button.snp.makeConstraints { make in
+                make.width.equalTo(button.snp.height)
+            }
+        }
+        
+        headerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.centerY.equalTo(stackView.snp.centerY)
+            make.trailing.equalTo(stackView.snp.leading).offset(-24)
+            make.width.equalTo(68)
+        }
+    }
+    
+    private func buildButton(text: String, cornerMask: CACornerMask) -> UIButton {
+        let button = UIButton()
+        button.setTitle(text, for: .normal)
+        button.titleLabel?.font = ThemeFont.bold(ofSize: 20)
+        button.backgroundColor = ThemeColor.primary
+        button.tintColor = .white
+        button.addRoundedCorners(corners: cornerMask, radius: 8.0)
+        return button
     }
 }
